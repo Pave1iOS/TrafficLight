15859ecdaaeb1ffd4d2355a25d136aef08690d5b
Pavel Gribachev <PGribachev@yandex.ru>
2023-11-05 18:08:02 +0300

    logic of the application changing colors when you press a button

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,32 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var redSignalView: UIView!
-    
     @IBOutlet weak var yellowSignalView: UIView!
-    
     @IBOutlet weak var greenSignalView: UIView!
-    
     @IBOutlet weak var changeColorButton: UIButton!
-        
+    
+    let cornerRadius: CGFloat = 50
+    var numberOfClicks = 0
+            
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         changeColorButton.layer.cornerRadius = 10
-                
+        
+        redSignalView.layer.cornerRadius = cornerRadius
+        yellowSignalView.layer.cornerRadius = cornerRadius
+        greenSignalView.layer.cornerRadius = cornerRadius
     }
-
     
     @IBAction func changeColorsDidTappedButton() {
         changeColorButton.setTitle("NEXT", for: .normal)
-        
-        
-        
+        numberOfClicks += 1
+            
+            switch numberOfClicks {
+            case 1:
+                redSignalView.alpha = 1
+                yellowSignalView.alpha = 0.3
+                greenSignalView.alpha = 0.3
+            case 2:
+                redSignalView.alpha = 0.3
+                yellowSignalView.alpha = 1
+                greenSignalView.alpha = 0.3
+            case 3:
+                redSignalView.alpha = 0.3
+                yellowSignalView.alpha = 0.3
+                greenSignalView.alpha = 1
+                numberOfClicks = 0
+            default:
+                break
+            }
     }
-    
-    
-    
-
 }
 
