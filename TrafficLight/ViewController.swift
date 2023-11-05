dc5257444f9e9debc86c06f5bfb1b7198af62010
Pavel Gribachev <PGribachev@yandex.ru>
2023-11-05 15:56:15 +0300

    Arranged interface elements and created their Outlet and Action

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSignalView: UIView!
+    
+    @IBOutlet weak var yellowSignalView: UIView!
+    
+    @IBOutlet weak var greenSignalView: UIView!
+    
+    @IBOutlet weak var changeColorButton: UIButton!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+                
     }
 
+    
+    @IBAction func changeColorsDidTappedButton() {
+        changeColorButton.setTitle("NEXT", for: .normal)
+        
+        
+        
+    }
+    
+    
+    
 
 }
 
